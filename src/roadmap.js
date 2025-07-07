32060b50cc8290b82daf59e96069ce4e2ad39610
adeelahmed057 <adeelnazir2018@gmail.com>
2025-07-07 22:18:30 +0500

    change the buy now button color

--- src/roadmap.js
+++ src/roadmap.js
@@ -1,23 +1,23 @@
-import './roadmap.css';
-import Navbar from './common/navbar';
-import Footer from './common/footer';
-import { useEffect } from 'react';
+import "./roadmap.css";
+import Navbar from "./common/navbar";
+import Footer from "./common/footer";
+import { useEffect } from "react";
 // import { Swiper, SwiperSlide } from 'swiper/react';
 // import { Navigation, Pagination } from 'swiper/modules';
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 function Roadmap() {
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    return (
-        <>
-            <Navbar />
-            {/* <section className="roadMap" id="roadmap">
+  return (
+    <>
+      <Navbar />
+      {/* <section className="roadMap" id="roadmap">
                 <h1 className="roadMapH1">Roadmap</h1>
                 <p className="roadMapP1">2025: Building the Future, Quarter by Quarter</p>
                 <div className="container roadmapMTSlider">
@@ -97,110 +97,115 @@ function Roadmap() {
                     </Swiper>
                 </div>
             </section> */}
-            <section className='roadMapBg'>
-                <div className='container'>
-                    <p className='pt-5 text-white'>Home  {'>>'} roadmap</p>
-                </div>
-                <h1 className='text-center roadmaph1' style={{ color: "#00F0FF" }}>Roadmap</h1>
-                <p className='text-white text-center roadmapp1'>2025-2026: Building the Future, Quarter by Quarter</p>
-                <div className="timeline">
-                    <div className="container-timeline left">
-                        <div className="content">
-                            <h2 style={{ color: "#00F0FF" }}>2025 - QUARTER 3</h2>
-                            <ul>
-                                {/* <li style={{ listStyle: 'none', marginLeft: -20 + 'px' }}><i className="bi bi-check"></i> Create Website
+      <section className="roadMapBg">
+        <div className="container">
+          <p className="pt-5 text-white">Home {">>"} roadmap</p>
+        </div>
+        <h1 className="text-center roadmaph1" style={{ color: "#00F0FF" }}>
+          Roadmap
+        </h1>
+        <p className="text-white text-center roadmapp1">
+          2025-2026: Building the Future, Quarter by Quarter
+        </p>
+        <div className="timeline">
+          <div className="container-timeline left">
+            <div className="content">
+              <h2 style={{ color: "#00F0FF" }}>2025 - QUARTER 3</h2>
+              <ul>
+                {/* <li style={{ listStyle: 'none', marginLeft: -20 + 'px' }}><i className="bi bi-check"></i> Create Website
                                 </li> */}
-                                <li >Create Blockchain (testnet)
-                                </li>
-                                <li>Create All Social Media Accounts
-                                </li>
-                                <li >Initial Offering
-                                </li>
-                                <li >Create All Social Media Accounts
-                                </li>
-                                <li >BlockEX Staking Mobile and Web App</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="container-timeline right">
-                        <div className="content">
-                            <h2 style={{ color: "#00F0FF" }}>2025 - QUARTER 4</h2>
-                            <ul>
-                                <li>Start Development on Centralized Exchange
-                                </li>
-                                <li>Listing Our Coin with Exchange
-                                </li>
-                                <li>Partnerships and Collaborations</li>
-
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="container-timeline left">
-                        <div className="content">
-                            <h2 style={{ color: "#00F0FF" }}>2026 - QUARTER 1
-                            </h2>
-                            <ul>
-                                <li>BlockEx DEX Launch
-                                </li>
-                                <li>BlockEx Centralized Exchange Launch
-                                </li>
-                                <li>Listing Coin in More exchanges
-                                </li>
-                                <li>BlockEx Mobile EVM Wallet</li>
-
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="container-timeline right">
-                        <div className="content">
-                            <h2 style={{ color: "#00F0FF" }}>2026 - QUARTER 2</h2>
-                            <ul>
-                                <li>BlockEx Mastercard Launch
-                                </li>
-                                <li>BlockEx Card Platform Launch
-                                </li>
-                                <li>BlockEx Vendor Program</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="container-timeline left">
-                        <div className="content">
-                            <h2 style={{ color: "#00F0FF" }}>2026 - QUARTER 3
-                            </h2>
-                            <ul>
-                                <li>Crypto Payment Gateway
-
-                                </li>
-                                <li>Listing BlockEx coin in more Top exchange
-                                </li>
-
-
-                            </ul>
-                        </div>
-                    </div>
+                <li>Create Blockchain (testnet)</li>
+                <li>Create All Social Media Accounts</li>
+                <li>Initial Offering</li>
+                <li>Create All Social Media Accounts</li>
+                <li>BlockEX Staking Mobile and Web App</li>
+              </ul>
+            </div>
+          </div>
+          <div className="container-timeline right">
+            <div className="content">
+              <h2 style={{ color: "#00F0FF" }}>2025 - QUARTER 4</h2>
+              <ul>
+                <li>Start Development on Centralized Exchange</li>
+                <li>Listing Our Coin with Exchange</li>
+                <li>Partnerships and Collaborations</li>
+              </ul>
+            </div>
+          </div>
+          <div className="container-timeline left">
+            <div className="content">
+              <h2 style={{ color: "#00F0FF" }}>2026 - QUARTER 1</h2>
+              <ul>
+                <li>BlockEx DEX Launch</li>
+                <li>BlockEx Centralized Exchange Launch</li>
+                <li>Listing Coin in More exchanges</li>
+                <li>BlockEx Mobile EVM Wallet</li>
+              </ul>
+            </div>
+          </div>
+          <div className="container-timeline right">
+            <div className="content">
+              <h2 style={{ color: "#00F0FF" }}>2026 - QUARTER 2</h2>
+              <ul>
+                <li>BlockEx Mastercard Launch</li>
+                <li>BlockEx Card Platform Launch</li>
+                <li>BlockEx Vendor Program</li>
+              </ul>
+            </div>
+          </div>
+          <div className="container-timeline left">
+            <div className="content">
+              <h2 style={{ color: "#00F0FF" }}>2026 - QUARTER 3</h2>
+              <ul>
+                <li>Crypto Payment Gateway</li>
+                <li>Listing BlockEx coin in more Top exchange</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <section className="contactus" style={{ marginTop: 250 + "px" }}>
+          <section
+            style={{
+              backgroundImage: "linear-gradient(to left, #00F0FF, #00C4F4)",
+            }}
+          >
+            <div className="container">
+              <div
+                className="position-relative"
+                style={{ paddingTop: 70 + "px", paddingBottom: 70 + "px" }}
+              >
+                <div className="d-flex flex-wrap">
+                  <div className="part1">
+                    <h1 style={{ color: "#000" }}>$500,000 Raised</h1>
+                    <p style={{ color: "rgba(10, 11, 13, 0.6)" }}>
+                      Join the BlockEx crypto presale, engage with the project,
+                      <br /> and see your contribution grow to new heights.
+                    </p>
+                    {/* <button className='btn buyBtn'>Buy Now</button> */}
+                    <a
+                      href="https://exchangeblockex.com/launchpad"
+                      target="_blank"
+                      className="btn btnBuy m-0"
+                      style={{background:"#171717"}}
+                    >
+                      Buy Now
+                    </a>
+                  </div>
+                  <div className="part2">
+                    <img
+                      src="/assets/images/contact_coin.png"
+                      className="img-fluid setImgPosition"
+                      alt=""
+                    />
+                  </div>
                 </div>
-                <section className="contactus" style={{ marginTop: 250 + 'px' }}>
-                    <section style={{ backgroundImage: 'linear-gradient(to left, #00F0FF, #00C4F4)' }}>
-                        <div className="container">
-                            <div className="position-relative" style={{ paddingTop: 70 + 'px', paddingBottom: 70 + 'px' }}>
-                                <div className="d-flex flex-wrap">
-                                    <div className="part1">
-                                        <h1 style={{ color: "#000" }}>$500,000 Raised</h1>
-                                        <p style={{ color: "rgba(10, 11, 13, 0.6)" }}>Join the BlockEx crypto presale, engage with the project,<br /> and see your contribution grow to new heights.</p>
-                                        {/* <button className='btn buyBtn'>Buy Now</button> */}
-                                        <button className='btn howToBuy text-white' style={{ background: "#000" , borderColor:"none"}}>How to Buy?</button>
-                                    </div>
-                                    <div className='part2'>
-                                        <img src='/assets/images/contact_coin.png' className='img-fluid setImgPosition'  alt=''/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                </section>
-            </section>
-            <Footer />
-        </>
-    )
+              </div>
+            </div>
+          </section>
+        </section>
+      </section>
+      <Footer />
+    </>
+  );
 }
-export default Roadmap
+export default Roadmap;
--- src/tokenomics.js
+++ src/tokenomics.js
@@ -1,95 +1,129 @@
-import Navbar from './common/navbar';
-import Footer from './common/footer';
-import { useEffect } from 'react';
+import Navbar from "./common/navbar";
+import Footer from "./common/footer";
+import { useEffect } from "react";
 function Topkenomics() {
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
-    return (
-        <>
-            <Navbar />
-            <div className='roadMapBg'>
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <>
+      <Navbar />
+      <div className="roadMapBg">
+        <div className="container">
+          <p className="mt-5 text-white">Home {">>"} Tokenomics</p>
+        </div>
+        <section className="" id="tokenomics">
+          <h1 className="TopkenomicsH1">TOKENOMICS</h1>
+          <p className="TopkenomicsP1">Tokenomics of BlockEx Coin</p>
+          <p className="TopkenomicsP2">
+            This tokenomics is designed for sustainability and growth, ensuring
+            a balanced
+            <br /> supply and demand. With a strategic allocations
+          </p>
 
-                <div className='container'>
-                    <p className='mt-5 text-white'>Home  {'>>'} Tokenomics</p>
-                </div>
-                <section className="" id="tokenomics">
-
-                    <h1 className="TopkenomicsH1">TOKENOMICS</h1>
-                    <p className="TopkenomicsP1">Tokenomics of BlockEx Coin</p>
-                    <p className="TopkenomicsP2">This tokenomics is designed for sustainability and growth, ensuring a balanced<br /> supply and demand. With a strategic allocations</p>
-
-                    <div className="container">
-                        <div className="row justify-content-center TopkenomicsPadding">
-
-                            <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12">
-                                <div className="d-flex flex-column">
-                                    <div className="place-end position-relative">
-                                        <div>
-                                            <h2 className="TopkenomicsH2"><span className="box1"></span>Early Contribute
-                                                <span className="percentage">7.0%</span></h2>
-
-                                        </div>
-                                        <div>
-                                            <h2 className="TopkenomicsH2"><span className="box2"></span>Series A Funding  <span className="percentage">10.0%</span></h2>
-
-                                        </div>
-                                        <div>
-                                            <h2 className="TopkenomicsH2"><span className="box3"></span>Series B Funding  <span className="percentage">8.0%</span></h2>
-
-                                        </div>
-                                        <div>
-                                            <h2 className="TopkenomicsH2"><span className="box4"></span>Exchange Liquidity <span className="percentage">50.0%</span></h2>
-
-                                        </div>
-                                        <div>
-                                            <h2 className="TopkenomicsH2"><span className="box5"></span>Staking Subsidies<span className="percentage">10.0%</span></h2>
-
-                                        </div>
-                                        <div>
-                                            <h2 className="TopkenomicsH2"><span className="box6"></span>Marketing <span className="percentage">8.5%</span></h2>
-
-                                        </div>
-                                        <div>
-                                            <h2 className="TopkenomicsH2"><span className="box7"></span>Company<span className="percentage">3.5%</span></h2>
-                                        </div>
-                                        <div>
-                                            <h2 className="TopkenomicsH2"><span className="box8"></span>Team <span className="percentage">3.0%</span></h2>
-                                        </div>
-                                        {/* <div>
+          <div className="container">
+            <div className="row justify-content-center TopkenomicsPadding">
+              <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                <div className="d-flex flex-column">
+                  <div className="place-end position-relative">
+                    <div>
+                      <h2 className="TopkenomicsH2">
+                        <span className="box1"></span>Early Contribute
+                        <span className="percentage">7.0%</span>
+                      </h2>
+                    </div>
+                    <div>
+                      <h2 className="TopkenomicsH2">
+                        <span className="box2"></span>Series A Funding{" "}
+                        <span className="percentage">10.0%</span>
+                      </h2>
+                    </div>
+                    <div>
+                      <h2 className="TopkenomicsH2">
+                        <span className="box3"></span>Series B Funding{" "}
+                        <span className="percentage">8.0%</span>
+                      </h2>
+                    </div>
+                    <div>
+                      <h2 className="TopkenomicsH2">
+                        <span className="box4"></span>Exchange Liquidity{" "}
+                        <span className="percentage">50.0%</span>
+                      </h2>
+                    </div>
+                    <div>
+                      <h2 className="TopkenomicsH2">
+                        <span className="box5"></span>Staking Subsidies
+                        <span className="percentage">10.0%</span>
+                      </h2>
+                    </div>
+                    <div>
+                      <h2 className="TopkenomicsH2">
+                        <span className="box6"></span>Marketing{" "}
+                        <span className="percentage">8.5%</span>
+                      </h2>
+                    </div>
+                    <div>
+                      <h2 className="TopkenomicsH2">
+                        <span className="box7"></span>Company
+                        <span className="percentage">3.5%</span>
+                      </h2>
+                    </div>
+                    <div>
+                      <h2 className="TopkenomicsH2">
+                        <span className="box8"></span>Team{" "}
+                        <span className="percentage">3.0%</span>
+                      </h2>
+                    </div>
+                    {/* <div>
                                         <h2 className="TopkenomicsH2"><span className="box9"></span>Development : <span className="percentage">5%</span></h2>
                                     </div> */}
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 d-flex justify-content-center">
-                                <img src="assets/images/pie-05.svg" className="img-fluid" alt="img" />
-                            </div>
-                        </div>
-                    </div>
-                </section >
-                <section className="contactus" id="contactus">
-                    <section className="contactUsBg">
-                        <div className="container">
-                            <div className="position-relative">
-                                <div className="d-flex flex-wrap">
-                                    <div className="part1">
-                                        <h1>$500,000 Raised</h1>
-                                        <p>Join the BlockEx crypto presale, engage with the project,<br /> and see your contribution grow to new heights.</p>
-                                        <button className='btn buyBtn'>Buy Now</button>
-                                        <button className='btn howToBuy'>How to Buy?</button>
-                                    </div>
-                                    <div className='part2'>
-                                        <img src='/assets/images/contact_coin.png' className='img-fluid setImgPosition' />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                </section>
+                  </div>
+                </div>
+              </div>
+              <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 d-flex justify-content-center">
+                <img
+                  src="assets/images/pie-05.svg"
+                  className="img-fluid"
+                  alt="img"
+                />
+              </div>
             </div>
-            <Footer />
-        </>
-    )
+          </div>
+        </section>
+        <section className="contactus" id="contactus">
+          <section className="contactUsBg">
+            <div className="container">
+              <div className="position-relative">
+                <div className="d-flex flex-wrap">
+                  <div className="part1">
+                    <h1>$500,000 Raised</h1>
+                    <p>
+                      Join the BlockEx crypto presale, engage with the project,
+                      <br /> and see your contribution grow to new heights.
+                    </p>
+                    <a
+                      href="https://exchangeblockex.com/launchpad"
+                      target="_blank"
+                      className="btn btnBuy m-0"
+                      style={{ background: "#171717" }}
+                    >
+                      Buy Now
+                    </a>
+                  </div>
+                  <div className="part2">
+                    <img
+                      src="/assets/images/contact_coin.png"
+                      className="img-fluid setImgPosition"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </section>
+      </div>
+      <Footer />
+    </>
+  );
 }
-export default Topkenomics
+export default Topkenomics;
